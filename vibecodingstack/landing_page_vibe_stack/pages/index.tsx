52c24e7ed9fbfaa7415ef377a992cf97c3189171
orientohubone <orientohub@gmail.com>
2025-07-12 16:32:51 -0300

    Update index.tsx

--- vibecodingstack/landing_page_vibe_stack/pages/index.tsx
+++ vibecodingstack/landing_page_vibe_stack/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Hero } from "../components/Hero";
 import { MainStack } from "../components/MainStack";
 import { TechFrameworks } from "../components/TechFrameworks";
-import { iasvibecoding } from "../components/iasvibecoding";
+import { IasvibeCoding } from "../components/IasvibeCoding";
 import { CloudDatabases } from "../components/CloudDatabases";
 import { GitHubFlow } from "../components/GitHubFlow";
 import { DeployOptions } from "../components/DeployOptions";
@@ -15,7 +15,7 @@ export default function LandingPage() {
       <Hero />
       <MainStack />
       <TechFrameworks />
-      <iasvibecoding />
+      <IasvibeCoding />
       <CloudDatabases />
       <GitHubFlow />
       <DeployOptions />
